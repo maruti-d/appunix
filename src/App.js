d991e1ae6f975a3a44c4e2d519cff32f93d57ec5
Maruti Davari <abhi777j@gmail.com>
2025-11-21 09:35:38 +0530

    added # router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme/theme';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import Layout from './components/layout/Layout';
 import AppRoutes from './routes/AppRoutes';
 export default function App(){
 return(
 <ThemeProvider theme={theme}>
-<BrowserRouter basename="/">
+<HashRouter>
 <Layout><AppRoutes /></Layout>
-</BrowserRouter>
+</HashRouter>
 </ThemeProvider>
 );
 }
